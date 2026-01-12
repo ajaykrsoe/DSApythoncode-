e494a495a5181e36187eebb7d9d701e0873bb063
AJAY KUMAR <92604899+ajaykrsoe@users.noreply.github.com>
2026-01-12 19:13:09 +0530

    Refactor permutation function with backtracking
    Refactored permutation function for better clarity and added backtracking.

--- permutation.py
+++ permutation.py
@@ -1,3 +1,4 @@
+#M1
 def permutation(index,l):
     if(index<len(l)):
         for i in range(index,len(l)):
@@ -11,3 +12,22 @@ def permutation(index,l):
 s="ABCDE"
 l=list(s)
 permutation(0,l)
+
+
+
+
+
+#M2
+def permutation(index, arr):
+    if index == len(arr):
+        print(''.join(arr), end=" ")
+        return
+
+    for i in range(index, len(arr)):
+        arr[index], arr[i] = arr[i], arr[index]
+        permutation(index + 1, arr)
+        arr[index], arr[i] = arr[i], arr[index]  # backtrack
+
+s = "ABCDE"
+permutation(0, list(s))
+
