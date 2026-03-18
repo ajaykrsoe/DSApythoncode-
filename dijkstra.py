8e433735bdc3bee1c149b62e75efaad39ef50869
AJAY KUMAR <92604899+ajaykrsoe@users.noreply.github.com>
2026-03-18 10:08:59 +0530

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -79,56 +79,25 @@ class Solution:
 
 
 
-       import heapq
-import sys
+import heapq
 
-def dijkstra(adj, src):
-
-    V = len(adj)
-
-    # Min-heap (priority queue) storing pairs of (distance, node)
-    pq = []
-
-    dist = [sys.maxsize] * V
-
-    # Distance from source to itself is 0
-    dist[src] = 0
-    heapq.heappush(pq, (0, src))
-
-    # Process the queue until all reachable vertices are finalized
-    while pq:
-        d, u = heapq.heappop(pq)
-
-        # If this distance not the latest shortest one, skip it
-        if d > dist[u]:
-            continue
-
-        # Explore all neighbors of the current vertex
-        for v, w in adj[u]:
-
-            # If we found a shorter path to v through u, update it
-            if dist[u] + w < dist[v]:
-                dist[v] = dist[u] + w
-                heapq.heappush(pq, (dist[v], v))
-
-    # Return the final shortest distances from the source
-    return dist
-
-
-if __name__ == "__main__":
-    src = 0
-    
-    adj = [
-        [(1, 4), (2, 8)],
-        [(0, 4), (4, 6), (2, 3)],
-        [(0, 8), (3, 2), (1, 3)],
-        [(2, 2), (4, 10)],
-        [(1, 6), (3, 10)]
-    ]
-    
-    result = dijkstra(adj, src)
-    print(*result)
-     
-        
+class Solution:
+    def dijkstra(self, V, edges, src):
+        adj=[[] for e in range(V)]
+        for u,v,w in edges :
+            adj[u].append( (v,w))
+            adj[v].append((u,w))
+        dist=[float("inf") for e in range(V)]
+        dist[src]=0
+        heap=[(0,src) ]
+        while(heap) :
+            d,u=heapq.heappop(heap)
+            if(d>dist[u]) :
+                continue
+            for v,w in adj[u] :
+                if(dist[u]+w < dist[v]) :
+                    dist[v]=dist[u]+w
+                    heapq.heappush( heap,(dist[u]+w,v))
+        return dist
         
          
